cbee2c3664d258f5d8feb94f04310a4f90f34d24
Christian <christian4nasc@gmail.com>
2025-04-15 19:13:53 -0300

    Página de login adicionada + slides carrosel!

--- code.js
+++ code.js
@@ -1,4 +1,19 @@
+//MOTRAR A SENHA
 function Mostrar_senha(){
     const input = document.getElementById('senha');
     input.type = input.type === 'password'? 'text' : 'password';
 }
+
+
+//SLIDE CARROSEL
+let contador = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(() => {
+  contador++;
+  if (contador > 4) contador = 1;
+  document.getElementById("radio" + contador).checked = true;
+  document.querySelector('.slides-box').style.animation = 'none';
+  document.querySelector('.slides-box').offsetHeight;
+  document.querySelector('.slides-box').style.animation = null;
+}, 5000);
