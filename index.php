b0d56bafd8b0608b8ca7c74237febf8657187c55
italo_leonardo <italoleonardo69@gmail.com>
2025-05-27 19:59:43 -0300

    Revert "criaçaõ de validação de login e senha"
    This reverts commit 283c1ded2ca37844c9fa575e2977794a859a5204.

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-$tipo_usuario = $_SESSION['tipo_usuario'] ?? null;
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -14,62 +10,48 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? null;
 <body class="principal">
     
     <header class="header">
-        <div class="logo">
+
+        <picture class="logo">
             <a href="index.php">
                 <img class="logo_img" src="style/img/Logo.png" alt="LOGO DO SITE">
             </a>
-        </div>
+        </picture>
 
-        <nav>
+        <div>
+
             <ul class="nav_pc">
-                <?php if ($tipo_usuario): ?>
-                    <li><a href="php/logout.php">Logout</a></li>
-                    <li><a href="Meus_eventos.php">Meus eventos</a></li>
-                    <?php if ($tipo_usuario === 'admin'): ?>
-                        <li><a href="cadastro_eventos.php">Cadastrar um evento</a></li>
-                        <li><a href="validacao.php">Validação de eventos</a></li>
-                    <?php endif; ?>
-                <?php else: ?>
-                    <li><a href="login.php">Login</a></li>
-                <?php endif; ?>
-            </ul>
-        </nav>
 
-        <form action="#" class="search">
-            <input name="pesquisa" type="search" placeholder="Buscar Eventos">
-            <input list="cidades" id="cidade" name="cidade" placeholder="Digite ou selecione">
-            <datalist id="cidades">
-                <option value="Juazeiro do Norte">
-            </datalist>
-        </form>
+                <ol>
+                    <a href="login.php">Login</a>
+                </ol>
+
+                <ol>
+                    <a href="Meus_eventos.php">Meus eventos</a>
+                </ol>
+
+                <ol>
+                    <a href="cadastro_eventos.php">Cadastrar um evento</a>
+                </ol>
+
+                <ol>
+                    <a href="validacao.php">Validação de evetos</a>
+                </ol>
+            </ul>
+
+        </div>
 
         <div>
             <form action="#" class="search">
 
-                <div class="search_header">            
-                    <div>
-                        <input name="pesquisa" type="list" placeholder="Buscar Eventos">
-                    </div>
-
-
-                    <div>
-                        <input list="cidades" id="cidade" name="cidade" placeholder="Digite ou selecione">
-                        <datalist id="cidades">
-                        <option value="Juazeiro do Norte">
-                        </datalist>
-                    </div>
+                <div>
+                    <input name="pesquisa" type="list" placeholder="Buscar Eventos">
                 </div>
 
                 <div>
-                    <ul>
-                        <ol>
-                            <picture class="login_icone" >
-                                <a href="login.php">
-                                    <img class="login_icone_img" src="style/img/user-interface.png" alt="Imagem de login" >
-                                </a>
-                            </picture>
-                        </ol>
-                    </ul>
+                    <input list="cidades" id="cidade" name="cidade" placeholder="Digite ou selecione">
+                    <datalist id="cidades">
+                      <option value="Juazeiro do Norte">
+                    </datalist>
                 </div>
 
             </form>
@@ -81,69 +63,76 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? null;
                 <a href="Meus_eventos.php">Meus eventos</a>
                 <a href="#">Cadastrar eventos</a>
             </div>
-        </nav>
+        </div>
     </header>
 
     <main>
-        <div class="slider_back">
-            <div class="slider">
-                <div class="slider-content">
-                    
-                    <input type="radio" name="btn-radio" id="radio1" checked>
-                    <input type="radio" name="btn-radio" id="radio2">
-                    <input type="radio" name="btn-radio" id="radio3">
-                    <input type="radio" name="btn-radio" id="radio4">
-            
-                    <div class="slides-box">
-                        <div class="slide-box">
-                            <img src="style/img/Expocrato_Banner.jpg" alt="Expocrato">
-                        </div>
-                        <div class="slide-box">
-                            <img src="style/img/Design sem nome.png" alt="Pau da Bandeira">
-                        </div>
-                        <div class="slide-box">
-                            <img src="style/img/Juaforro.jpg" alt="Jua Forró">
-                        </div>
-                        <div class="slide-box">
-                            <img src="style/img/Romaria.webp" alt="Romaria de juazerio">
-                        </div>
+
+        <div class="slider">
+            <div class="slider-content">
+                
+                <input type="radio" name="btn-radio" id="radio1" checked>
+                <input type="radio" name="btn-radio" id="radio2">
+                <input type="radio" name="btn-radio" id="radio3">
+                <input type="radio" name="btn-radio" id="radio4">
+        
+                <div class="slides-box">
+                    <div class="slide-box">
+                        <img src="style/img/Expocrato_Banner.jpg" alt="Expocrato">
+                    </div>
+                    <div class="slide-box">
+                        <img src="style/img/Design sem nome.png" alt="Pau da Bandeira">
+                    </div>
+                    <div class="slide-box">
+                        <img src="style/img/Juaforro.jpg" alt="Jua Forró">
+                    </div>
+                    <div class="slide-box">
+                        <img src="style/img/Romaria.webp" alt="Romaria de juazerio">
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div class="nav-manual" id="nav-slider">
-                <label for="radio1" class="manual-btn"></label>
-                <label for="radio2" class="manual-btn"></label>
-                <label for="radio3" class="manual-btn"></label>
-                <label for="radio4" class="manual-btn"></label>
+        <div class="nav-manual" id="nav-slider">
+            <label for="radio1" class="manual-btn"></label>
+            <label for="radio2" class="manual-btn"></label>
+            <label for="radio3" class="manual-btn"></label>
+            <label for="radio4" class="manual-btn"></label>
+        </div>
+
+        <div class="organizar-datas">
+            <div class="datas-background">
+                <div class="datas">
+                    <span>01/04</span>
+                    <span>Terça-feira</span>
+                </div>
+
+                <div class="datas">
+                    <span>02/04</span>
+                    <span>Quarta-feira</span>
+                </div>
+
+                <div class="datas">
+                    <span>03/04</span>
+                    <span>Quinta-feira</span>
+                </div>
+
+                <div class="datas">
+                    <span>04/04</span>
+                    <span>Sexta-feira</span>
+                </div>
+
+                <div class="datas">
+                    <span>CALENDÁRIO</span>
+                </div>
             </div>
         </div>
 
+        <div class="eventos-maiores">
 
+            <div class="background-eventos">
 
-
-        <section class="organizar-datas">
-            <div class="datas-background">
-                <div class="datas"><span>01/04</span><span>Terça-feira</span></div>
-                <div class="datas"><span>02/04</span><span>Quarta-feira</span></div>
-                <div class="datas"><span>03/04</span><span>Quinta-feira</span></div>
-                <div class="datas"><span>04/04</span><span>Sexta-feira</span></div>
-                <div class="datas"><span>CALENDÁRIO</span></div>
-            </div>
-        </section>
-
-        <section class="eventos-maiores">
-
-            <article class="background-eventos">
                 <h2>Expocrato 01/04</h2>
-                <picture><img src="style/img/Expocrato_miniatura.jpeg" alt="Imagem do evento Expocrato"></picture>
-                <p>
-                    A espera acabou! O maior evento de música e cultura do interior do Ceará já tem data marcada
-                    para agitar o Cariri. A Expocrato 2025 acontece de 11 a 20 de julho, no Parque de Exposições
-                    Pedro Felício Cavalcante, no Crato.
-                </p>
-                <a href="Descricao.html?evento=expocrato_ativo"><button>Ver Detalhes</button></a>
-            </article>
 
                 <picture>
                     <img src="style/img/Expocrato_miniatura.jpeg" alt="IMAGEM DO EVENTO">
@@ -155,7 +144,7 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? null;
                         com os maiores artistas do Brasil em um espetáculo de luz, som e emoção.
                     </p>
 
-                    <a href="Descricao.php?evento=expocrato_ativo">
+                    <a href="Descricao.html?evento=expocrato_ativo">
                         <button>Ver Detalhes</button>
                     </a>
             </div>
@@ -163,14 +152,6 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? null;
             <div class="background-eventos">
 
                 <h2>Pau da Bandeira 02/04</h2>
-                <picture><img src="style/img/Paudabandeira_miniatura.jpeg" alt="Imagem do evento Pau da Bandeira"></picture>
-                <p>
-                    A Festa do Pau da Bandeira é uma tradicional celebração em homenagem a Santo Antônio,
-                    padroeiro de Barbalha, no Ceará. Acontece anualmente com corte, transporte e hasteamento
-                    do tronco, manifestações culturais e shows.
-                </p>
-                <a href="Descricao.html?evento=paudabandeira_ativo"><button>Ver Detalhes</button></a>
-            </article>
 
                 <picture>
                     <img src="style/img/Paudabandeira_miniatura.jpeg" alt="IMAGEM DO EVENTO">
@@ -186,7 +167,7 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? null;
                         do Cariri cearense.
                     </p>
 
-                    <a href="Descricao.php?evento=paudabandeira_ativo">
+                    <a href="Descricao.html?evento=paudabandeira_ativo">
                         <button>Ver Detalhes</button>
                     </a>
         
@@ -195,13 +176,6 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? null;
             <div class="background-eventos">
                 
                 <h2>Juaforró 03/04</h2>
-                <picture><img src="style/img/Juaforro_miniatura.jpeg" alt="Imagem do evento Juaforró"></picture>
-                <p>
-                    O Juaforró 2024 acontece de 19 a 23 de junho no Parque de Eventos Padre Cícero e é gratuito.
-                    Com mais de 20 atrações musicais, quadrilhas, comidas típicas e cultura.
-                </p>
-                <a href="Descricao.html?evento=juaforro_ativo"><button>Ver Detalhes</button></a>
-            </article>
 
                 <picture>
                     <img src="style/img/Juaforro_miniatura.jpeg" alt="IMAGEM DO EVENTO">
@@ -211,7 +185,7 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? null;
                         O Juaforró 2024, tradicional festa junina de Juazeiro do Norte, acontece de 19 a 23 de junho no Parque de Eventos Padre Cícero e é gratuito. A programação inclui mais de 20 atrações musicais como Fagner, Taty Girl, Solange Almeida, Mano Walter, Banda Líbanos e muitos outros. Além dos shows, o evento conta com apresentações de 28 quadrilhas juninas, barracas com comidas típicas e atividades culturais.
                     </p>
 
-                    <a href="Descricao.php?evento=juaforro_ativo">
+                    <a href="Descricao.html?evento=juaforro_ativo">
                         <button>Ver Detalhes</button>
                     </a>
             </div>
@@ -219,5 +193,6 @@ $tipo_usuario = $_SESSION['tipo_usuario'] ?? null;
         </div>
 
     </main>
+
 </body>
 </html>
